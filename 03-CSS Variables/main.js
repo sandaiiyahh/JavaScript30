6f04947e2fa0a48d21504e163ffdb51a3eb05c53
Sandy Dai <sandydai98@gmail.com>
2021-04-16 16:50:51 -0400

    feat(Day3): completees css property updates with js

---
+++ 03-CSS Variables/main.js
@@ -0,0 +1,17 @@
+// Selecting all the input element under the .controls class
+const inputs = document.querySelectorAll('.controls input');
+console.log('inputs -->', inputs);
+
+function handleUpdate() {
+  // Taking the 'px' from the spacing/blur input OR if it's a color picker input, suffix is an empty string
+  const suffix = this.dataset.sizing || '';
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix // adds suffix ('px') onto the end of value
+  );
+}
+
+// Listens for changes to the value of any of the input elements
+inputs.forEach((input) => input.addEventListener('input', handleUpdate));
+inputs.forEach((input) => input.addEventListener('mousemove', handleUpdate));
