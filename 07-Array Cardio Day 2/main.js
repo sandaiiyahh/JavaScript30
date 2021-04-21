3a7dd531cdc4415835c80db33467a3daf28ffce6
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-04-21 16:37:54 -0400

    feat(Day7): finish findIndex and delete method

--- 07-Array Cardio Day 2/main.js
+++ 07-Array Cardio Day 2/main.js
@@ -20,21 +20,46 @@ const comments = [
 const isAdult = people.some(
   (person) => new Date().getFullYear() - person.year >= 19
 );
-console.log({ isAdult });
+console.log('Is at least one person an adult?:');
+console.table({ isAdult });
 
 // .every() checks for everyone
 // Is EVERYONE 19 or older?
 
+console.log('Is everyone an adult?');
 const allAdults = people.every(
   (person) => new Date().getFullYear() - person.year >= 19
 );
-console.log({ allAdults });
+console.table({ allAdults });
 
 /* ------Find------- */
-
 // .find() is like filter, but instead returns just the one you are looking for
 // Find the comment with the ID of 823423
+const comment = comments.find((comment) => comment.id === 823423);
 
-// Array.prototype.findIndex()
-// Find the comment with this ID
-// delete the comment with the ID of 823423
+console.log('Find comment with ID 823423:');
+console.table(comment);
+
+/* ------findIndex------- */
+// .findIndex() helps you find where something is in the array
+// Find the comment with ID 823423
+const index = comments.findIndex((comment) => comment.id === 823423);
+
+console.log('Find index with comment ID 823423:');
+console.log(index);
+
+/* ------Delete Something From Array------- */
+// Delete the comment with the ID of 823423
+// Method 1: .splice(), although it mutates the original comments array
+// comments.splice(index, 1);
+
+// console.log('Delete that comment from array:');
+// console.table(comments);
+
+// Method 2: Slice & Spread operator, which creates a new array and stores the updated values
+const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
+console.log('Delete that comment from array:');
+console.table(newComments);
+
+console.log('Old comments array:');
+console.table(comments);
