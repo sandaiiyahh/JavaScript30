d47ee6cf6c937d8a0fad84b0ce1f91ceef4407a8
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-04-29 22:01:54 -0400

    feat: images sappear when scroll halfway and disappear after scroll past

--- 13-Slide In On Scroll/main.js
+++ 13-Slide In On Scroll/main.js
@@ -1,3 +1,4 @@
+// Run every time we scroll, but only runs checkSlide once every certain amount of time
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function () {
@@ -13,3 +14,29 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+// Select all images that we want to slide in:
+const sliderImages = document.querySelectorAll('.slide-in');
+
+// Function that runs every time user scrolls
+function checkSlide(e) {
+  // Loop over every single image & figure out when it needs to be shown
+  sliderImages.forEach((sliderImage) => {
+    // Find out how far page is being scrolled down
+    // window.scrollY = how much you are scrolled down from the top
+    // slideInAt = how much you are scrolled down from the bottom (doesn't start from 0. Ascends)
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2; // pixel level at which photo will slide in at (halfway)
+    const imageBottom = sliderImage.offsetTop + sliderImage.height; // how far bottom of image is from top of window
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      // For when we have scrolled past/have not got to the image
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
