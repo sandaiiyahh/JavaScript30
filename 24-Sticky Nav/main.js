518d64443735269d4b67059b383e6585c6406298
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-13 16:08:24 -0400

    feat: adds padding top to account for CSS fixed position

--- 24-Sticky Nav/main.js
+++ 24-Sticky Nav/main.js
@@ -1,13 +1,17 @@
 const nav = document.querySelector('#main');
+// Grab where top of nav is
+const topOfNav = nav.offsetTop;
 
 // Runs every single time the page scrolls
 function fixNav() {
-  // Grab where top of nav is
-  const topOfNav = nav.offsetTop;
   // At the point where scroll is at the nav, make the navbar fixed
   if (window.scrollY >= topOfNav) {
+    // Have to account for the CSS fixed positioning
+    document.body.style.paddingTop = nav.offsetHeight + 'px'; // how tall the height of the navbar is
+
     document.body.classList.add('fixed-nav'); // add to body so you can always target its children
   } else {
+    document.body.style.paddingTop = 0;
     document.body.classList.remove('fixed-nav');
   }
 }
