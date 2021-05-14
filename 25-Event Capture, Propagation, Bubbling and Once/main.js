7caeda2a562136574200b8018ea617b9d573b5d1
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-14 17:51:21 -0400

    feat(main): add propagation, bevent capture, and once to event listeners

---
+++ 25-Event Capture, Propagation, Bubbling and Once/main.js
@@ -0,0 +1,22 @@
+// Select all the divs
+const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
+
+function logText(e) {
+  // Clicking the most inner body clicks on everything else because it wraps around it
+  console.log(this.classList.value);
+  e.stopPropagation(); // stop bubbling the event up (no longer trigger events on parents)
+}
+
+// Capture = true: runs on the way down (one, two, three)
+divs.forEach((div) =>
+  div.addEventListener('click', logText, { capture: false })
+);
+
+button.addEventListener(
+  'click',
+  () => {
+    console.log('Clicked! Just once though right?');
+  },
+  { once: true }
+);
