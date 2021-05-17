55d9dae04e1ab5cde382a62c6982cbd1e229cb6c
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-17 15:15:42 -0400

    feat(main): white dropdown bg div sizes according to dropdown div coords

--- 26-Stripe Follow Along Nav/main.js
+++ 26-Stripe Follow Along Nav/main.js
@@ -15,6 +15,24 @@ function handleEnter() {
   setTimeout(() => this.classList.add('trigger-enter-active'), 150); // arrow function -> 'this' is inherited from parent
   // Add 'open' class to white dropdown background div
   background.classList.add('open');
+
+  // Grab dropdown of the one that appeared after hover
+  const dropdown = this.querySelector('.dropdown'); // dropdown courses vs dropdown dropdown3 vs dropdown dropdown1..
+
+  // Get coordinates from dropdown
+  const dropdownCoords = dropdown.getBoundingClientRect();
+
+  // Get coordinates of nav as well (so dropdownCoords are in relation to nav. In case something new gets added)
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+  };
+
+  // Now that we have width & height, we can set the white dropdown bg to it
+  background.style.setProperty('width', `${coords.width}px`);
+  background.style.setProperty('height', `${coords.height}px`);
 }
 
 // Remove classes once you hover away from li
