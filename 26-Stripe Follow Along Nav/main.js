4319498f8085396c848260d1e304ba3bf6d493a6
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-17 14:51:55 -0400

    feat(main): add classes inside handleEnter

--- 26-Stripe Follow Along Nav/main.js
+++ 26-Stripe Follow Along Nav/main.js
@@ -7,9 +7,12 @@ const background = document.querySelector('.dropdownBackground');
 // Grab the nav
 const nav = document.querySelector('.top');
 
-//
+// Add class to show dropdown content
 function handleEnter() {
-  console.log('ENTER');
+  // Add 'trigger-enter' class to li
+  this.classList.add('trigger-enter');
+  // Add 'trigger-enter-active' class after 150 ms
+  setTimeout(() => this.classList.add('trigger-enter-active'), 150); // arrow function -> 'this' is inherited from parent
 }
 
 function handleLeave() {
