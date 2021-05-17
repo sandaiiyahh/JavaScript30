a3822d8ed11bd614bcba27bd77939878354a4d4b
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-17 14:58:59 -0400

    feat(main): incorporates white bdropdown background div to functions

--- 26-Stripe Follow Along Nav/main.js
+++ 26-Stripe Follow Along Nav/main.js
@@ -13,11 +13,15 @@ function handleEnter() {
   this.classList.add('trigger-enter');
   // Add 'trigger-enter-active' class after 150 ms
   setTimeout(() => this.classList.add('trigger-enter-active'), 150); // arrow function -> 'this' is inherited from parent
+  // Add 'open' class to white dropdown background div
+  background.classList.add('open');
 }
 
 // Remove classes once you hover away from li
 function handleLeave() {
   this.classList.remove('trigger-enter', 'trigger-enter-active');
+  // Remove 'open' class from white dropdown background div
+  background.classList.remove('open');
 }
 
 // Listen for mouseenter and leave on each of triggers and fire functions as a response
