f0ed11c2e0077dcf71890aa2ea0cfc8017a72d35
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-18 17:27:52 -0400

    feat(main): adds startX and scrollLeft into mousedown event

--- 27-Click and Drag/main.js
+++ 27-Click and Drag/main.js
@@ -4,15 +4,19 @@ const slider = document.querySelector('.items');
 // Flag that tells if user is clicking down or not
 let isDown = false;
 
-let startX;
-let scrollLeft;
+let startX; // records position before the mouse dragging starts
+let scrollLeft; // records initial scroll position
 
 // Event listeners for each mouse event
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', (e) => {
   isDown = true;
   // Set an 'active' class once someone's mouse is down
   slider.classList.add('active');
+  // Get position of mousedown, offsetting any potential margins
+  startX = e.pageX - slider.offsetLeft;
+  // Get initial scroll position
+  scrollLeft = slider.scrollLeft;
 });
 
 slider.addEventListener('mouseleave', () => {
@@ -29,5 +33,4 @@ slider.addEventListener('mouseup', () => {
 
 slider.addEventListener('mousemove', () => {
   if (!isDown) return; // stops function from running. Won't run if isDown is false
-  console.log('Do work');
 });
