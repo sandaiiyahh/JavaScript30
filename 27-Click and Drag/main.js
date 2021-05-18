c1fa7900ea1dc689831c2c67f1ba2161109bc6ea
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-18 17:21:13 -0400

    feat(main): incorporate active class in mouse events

--- 27-Click and Drag/main.js
+++ 27-Click and Drag/main.js
@@ -11,20 +11,23 @@ let scrollLeft;
 
 slider.addEventListener('mousedown', () => {
   isDown = true;
+  // Set an 'active' class once someone's mouse is down
+  slider.classList.add('active');
 });
 
 slider.addEventListener('mouseleave', () => {
   // When someone leaves, set isDown to false
   isDown = false;
+  slider.classList.remove('active');
 });
 
 slider.addEventListener('mouseup', () => {
   // When someone's mouse is up, set isDown to false
   isDown = false;
+  slider.classList.remove('active');
 });
 
 slider.addEventListener('mousemove', () => {
   if (!isDown) return; // stops function from running. Won't run if isDown is false
-  console.log('isDown -->', isDown);
   console.log('Do work');
 });
