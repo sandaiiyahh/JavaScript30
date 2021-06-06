bc850d18e1c2f041f1ec8ae9f4783aa2dbc8c3b6
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-06-06 17:24:44 -0400

    feat(30): adds function that makes moles pop up and disappear

--- 30-Whack a Mole/main.js
+++ 30-Whack a Mole/main.js
@@ -3,6 +3,7 @@ const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 
 let lastHole;
+let timeUp = false; // determine when to stop moles from peeping
 
 // Function that gives us random amount of milliseconds
 function randomTime(min, max) {
@@ -24,4 +25,21 @@ function randomHole(holes) {
   // Save the previous hole
   lastHole = hole;
   return hole;
+}
+
+// Function that makes moles pop up and disappear
+function peep() {
+  // Get random hole and random amount of time
+  const time = randomTime(200, 1000); // Between 200 milliseconds and 1 second
+  const hole = randomHole(holes);
+
+  // Take hole and add 'up' class to it (animate mole in)
+  hole.classList.add('up');
+
+  // Make mole disappear after time is ran
+  setTimeout(() => {
+    hole.classList.remove('up');
+    // Only runs again if timeUp is set to false
+    if (!timeUp) peep();
+  }, time);
 }
